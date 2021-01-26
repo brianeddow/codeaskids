9aa004fafca1ae4a5c550d52000a5704fbae7878
Stephen Popovich <s.popovich@circlepoint.com>
2021-01-25 17:32:49 -0800

    update class to className within the accordion about FAQs

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -172,49 +172,49 @@ class About extends Component {
 						 <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
 							<h2>Frequently Asked Questions (FAQs)</h2>
 
-							<a href="#question-one" class="btn btn-info" data-toggle="collapse">When was this website created?</a>
-							<div id="question-one" class="collapse">
+							<a href="#question-one" className="btn btn-info" data-toggle="collapse">When was this website created?</a>
+							<div id="question-one" className="collapse">
 								<p><strong>A:</strong> The website launched on XXXXX, XX, 2021.</p>
 							</div>
                             <br/>
-							<a href="#question-two" class="btn btn-info" data-toggle="collapse">How can I help?</a>
-							<div id="question-two" class="collapse">
+							<a href="#question-two" className="btn btn-info" data-toggle="collapse">How can I help?</a>
+							<div id="question-two" className="collapse">
 								<p><strong>A:</strong> We run on selling advertisement space and volunteer writers to create fresh new content consistently. If you could contribute to one or both of these please <a href="">contact us</a>. Lastly, word of mouth share our website on social media or directly to friends and family.
 					                 We want to always do better, so recommendations are always welcome. Thank you!</p>
 							</div>
 							<br/>
-							<a href="#question-three" class="btn btn-info" data-toggle="collapse">Why a monkey and the jungle theme?</a>
-							<div id="question-three" class="collapse">
+							<a href="#question-three" className="btn btn-info" data-toggle="collapse">Why a monkey and the jungle theme?</a>
+							<div id="question-three" className="collapse">
 								<p><strong>A:</strong> The world of technology is a jungle. Welcome to our jungle and for the monkey...monkeys are awesome and curious.</p>
 							</div>
 							<br/>
-							<a href="#question-four" class="btn btn-info" data-toggle="collapse">What if I’m older than the age range (3<sup>rd</sup> to 12<sup>th</sup> graders)?</a>
-							<div id="question-four" class="collapse">
+							<a href="#question-four" className="btn btn-info" data-toggle="collapse">What if I’m older than the age range (3<sup>rd</sup> to 12<sup>th</sup> graders)?</a>
+							<div id="question-four" className="collapse">
 								<p><strong>A:</strong> We only recommend those age ranges. We write our content to cater to those age groups, but users of all ages can start their coding journey here!</p>
 							</div>
 							<br/>
-							<a href="#question-five" class="btn btn-info" data-toggle="collapse">What's the Monkey’s Name?</a>
-							<div id="question-five" class="collapse">
+							<a href="#question-five" className="btn btn-info" data-toggle="collapse">What's the Monkey’s Name?</a>
+							<div id="question-five" className="collapse">
 								<p><strong>A:</strong> Peels, make sure to wave hello when he appears on your screen with important or encouraging wordings.</p>
 							</div>
 							<br/>
-							<a href="#question-six" class="btn btn-info" data-toggle="collapse">Is the Website Accessible and 508 Compliant?</a>
-							<div id="question-six" class="collapse">
+							<a href="#question-six" className="btn btn-info" data-toggle="collapse">Is the Website Accessible and 508 Compliant?</a>
+							<div id="question-six" className="collapse">
 								<p><strong>A:</strong> We strive to meet these standards so all kids can get the most out of the knowledge we wish to share on our website. We are always open to suggestions on improving our accessibility or any special requests we will try to meet.</p>
 							</div>
 							<br/>
-							<a href="#question-seven" class="btn btn-info" data-toggle="collapse">Is the Website Accessible and 508 Compliant?</a>
-							<div id="question-seven" class="collapse">
+							<a href="#question-seven" className="btn btn-info" data-toggle="collapse">Is the Website Accessible and 508 Compliant?</a>
+							<div id="question-seven" className="collapse">
 								<p><strong>A:</strong> We strive to meet these standards so all kids can get the most out of the knowledge we wish to share on our website. We are always open to suggestions on improving our accessibility or any special requests we will try to meet.</p>
 							</div>
 							<br/>
-							<a href="#question-seven" class="btn btn-info" data-toggle="collapse">How can I volunteer?</a>
-							<div id="question-seven" class="collapse">
+							<a href="#question-seven" className="btn btn-info" data-toggle="collapse">How can I volunteer?</a>
+							<div id="question-seven" className="collapse">
 								<p><strong>A:</strong> Please go to our contact page and click the "Volunteer" button.</p>
 							</div>
 							<br/>
-							<a href="#question-seven" class="btn btn-info" data-toggle="collapse">Are there any rules for the website</a>
-							<div id="question-seven" class="collapse">
+							<a href="#question-seven" className="btn btn-info" data-toggle="collapse">Are there any rules for the website</a>
+							<div id="question-seven" className="collapse">
 								<p><strong>A:</strong> Yes, a few to help ensure everyone has a fun and safe experience. 1. If comments become a feature no foul language, bullying or negative behavior. 2. If you volunteer please take your responsibilities seriously. 3. Coding can be frustrating please do not take it out on us. </p>
 							</div>
 
